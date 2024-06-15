2bb183c685647cf2781e391d770b7f58b2c4ed92
Aftab48 <mdalam4884@gmail.com>
2024-06-15 13:09:52 +0530

    route update

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -3,6 +3,10 @@ import { stripe } from "@/lib/stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
+import { Resend } from "resend";
+// import OrderReceivedEmail from "@/components/emails/OrderReceivedEmail";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   try {
@@ -38,10 +42,9 @@ export async function POST(req: Request) {
       const billingAddress = session.customer_details!.address;
       const shippingAddress = session.shipping_details!.address;
 
-      await db.order.update({
+      const updatedOrder = await db.order.update({
         where: {
           id: orderId,
-          userId: userId,
         },
         data: {
           isPaid: true,
@@ -52,7 +55,7 @@ export async function POST(req: Request) {
               country: shippingAddress!.country!,
               postalCode: shippingAddress!.postal_code!,
               street: shippingAddress!.line1!,
-              state: shippingAddress!.state!,
+              state: shippingAddress!.state,
             },
           },
           billingAddress: {
@@ -62,11 +65,30 @@ export async function POST(req: Request) {
               country: billingAddress!.country!,
               postalCode: billingAddress!.postal_code!,
               street: billingAddress!.line1!,
-              state: billingAddress!.state!,
+              state: billingAddress!.state,
             },
           },
         },
       });
+
+      // await resend.emails.send({
+      //   from: "CaseCobra <mdalam4884@gmail.com>",
+      //   to: [event.data.object.customer_details.email],
+      //   subject: "Thanks for your order!",
+      //   react: OrderReceivedEmail({
+      //     orderId,
+      //     orderDate: updatedOrder.createdAt.toLocaleDateString(),
+      //     // @ts-ignore
+      //     shippingAddress: {
+      //       name: session.customer_details!.name!,
+      //       city: shippingAddress!.city!,
+      //       country: shippingAddress!.country!,
+      //       postalCode: shippingAddress!.postal_code!,
+      //       street: shippingAddress!.line1!,
+      //       state: shippingAddress!.state,
+      //     },
+      //   }),
+      // });
     }
 
     return NextResponse.json({ result: event, ok: true });
